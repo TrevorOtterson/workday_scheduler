19061e0dea42bfda57fc935b179f79dd085402ad
Trevor Otterson <totterson97@gmail.com>
2020-06-25 16:17:59 -0600

    removed excess code

--- script.js
+++ script.js
@@ -1,71 +1,13 @@
 // Variables
-// var header = $('.jumbotron')
-// var date = $('#currentDay')
-// var container = $('.container')
-// var timeSlot = $('.time-block')
-
-// $(document).ready(function () {
-
-//     // Pull times from moment.js
-//     time = setInterval(current, 1000)
-//     var current = moment().format('MMMM Do YYYY, h:mm:ss A')
-//     var hour24 = moment().format('H')
-
-//     // sets date text in header
-//     var dateHeader = (date)
-//     dateHeader.text(current)
-
-//     // Pulls data from local stoarage
-//     var storedData = JSON.parse(localStorage.getItem('storedData'))
-//     if (storedData !== null) {
-//         txtArea = storedData
-//     }
-
-//     for (var i = 9; i <= 17; i++) {
-//         // index for array use offset from hour
-//         var hour = i - 9
-//     }
-
-//     rowColor(timeSlot, i)
-//     // function to update row color
-//     function rowColor(row, i) {
-
-//         if (i < hour24) {
-//             // row.css('')
-//             row.css("background-color", "grey")
-//         }
-//         else if (i > hour24) {
-//             row.css("background-color", "rgb(41, 92, 0)")
-//         }
-//         else {
-//             row.css("background-color", "red")
-//         }
-//     }
-
-//     // saves to local storage
-//     // onclick function to listen for user clicks on plan area
-//     $(document).on('click', 'i', function(event) {
-//         event.preventDefault()
-
-//         var saveIndex = $(this).attr('save-id')
-//         var inputId = '#input-' + saveIndex
-//         var sValue = $(inputId).val()
-//         var txtArea = storedData
-//         console.log(txtArea)
-//         txtArea[{saveIndex}] = sValue
-//         localStorage.setItem('storedData', JSON.stringify(txtArea))
-//     })
-// })
-
-// Variables
 var header = $('.jumbotron')
 var date = $('#currentDay')
 var container = $('.container')
 var timeSlot = $('.time-block')
 $(document).ready(function () {
 
+setInterval(checkHours, 1000);
     function checkHours() {
-        $('.time-block').each(function () {
+        $('.time-block').each(function() {
          var blockHour = parseInt($(this).data('hour'));
          var currentHour = moment().hours();
      
@@ -81,8 +23,6 @@ $(document).ready(function () {
          }
         });
        }
-
-       setInterval(checkHours, 1000);
 
     // Pull times from moment.js
     var current = moment().format('MMMM Do YYYY, h:mm:ss a')
