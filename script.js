34ab391d5da415fd79e037be1eeb4bf072a5fe4f
Trevor Otterson <totterson97@gmail.com>
2020-06-25 16:27:29 -0600

    involving a for loop to pull text out of an array

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// Variables
-var header = $('.jumbotron')
-var date = $('#currentDay')
-var container = $('.container')
-var timeSlot = $('.time-block')
 $(document).ready(function () {
 
-setInterval(checkHours, 1000);
+    setInterval(checkHours, 1000);
     function checkHours() {
         $('.time-block').each(function() {
          var blockHour = parseInt($(this).data('hour'));
@@ -23,9 +18,8 @@ setInterval(checkHours, 1000);
          }
         });
        }
-
     // Pull times from moment.js
-    var current = moment().format('MMMM Do YYYY, h:mm:ss a')
+    var current = moment().format('MMMM Do YYYY, h:mm:ss A')
     var hour24 = moment().format('H');
     // sets date text in header
     var dateHeader = (date)
@@ -39,6 +33,7 @@ setInterval(checkHours, 1000);
         // index for array use offset from hour
         var index = hour - 9;
     }
+    /*
     timeSlot.each(function(){
         var row = $(this);
         var rowTime = $(this).attr("data-hour");
@@ -64,28 +59,53 @@ setInterval(checkHours, 1000);
             console.log("for some reason, none of the condidtions were filled.");   
         }
     });
-
+    */
+    for(var i=0; i<timeSlot.length; i++) {
+        var row = timeSlot[i];
+        //console.log(row);
+        var rowTime = $(row).attr("data-hour");
+        rowTime = parseInt(rowTime);
+        hour24 = parseInt(hour24);
+        //console.log(row);
+        console.log("rowTime: " + rowTime);
+        //console.log("hourOffset: " + hourOffset);
+        //console.log("hour24: " + hour24);
+        if (rowTime < hour24) {
+            console.log("should be past");
+            // $hourRow.css('')
+            $(row).addClass("past");
+        }
+        else if (rowTime === hour24) {
+            console.log("should be present");
+            $(row).addClass("present");
+        }
+        else if (rowTime > hour24) {
+            console.log("should be future");
+            $(row).addClass("future");
+        } else {
+            console.log("for some reason, none of the condidtions were filled.");   
+        }
+    };
+    /*
     rowColor(timeSlot, hour)
     // function to update row color
     function rowColor(row, hour) {
-
-        console.log(row);
-        console.log(hour);
-        console.log(hour24);
-        console.log(rowTime);
-
+        //console.log(row);
+        //console.log(hour);
+        //console.log(hour24);
+        //console.log(rowTime);
         if (hour < hour24) {
             // $hourRow.css('')
-            row.css("background-color", "grey")
+            row.css("background-color", "lightgrey")
         }
         else if (hour > hour24) {
             row.css("background-color", "rgb(41, 92, 0)")
         }
         else {
-            row.css("background-color", "red")
+            row.css("background-color", "lightred")
         }
     };
-
+    */
     // saves to local storage
     // onclick function to listen for user clicks on plan area
     $(document).on('click', 'i', function (event) {
@@ -97,4 +117,4 @@ setInterval(checkHours, 1000);
         txtArea[saveIndex] = sValue;
         localStorage.setItem('storedData', JSON.stringify(txtArea));
     });
-})
+});
