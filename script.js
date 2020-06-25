978b1f60e8a7afeaec1a24e19b2c4e960003d834
Trevor Otterson <totterson97@gmail.com>
2020-06-25 16:38:32 -0600

    deleted excess code

--- script.js
+++ script.js
@@ -39,33 +39,7 @@ $(document).ready(function () {
         // index for array use offset from hour
         var index = hour - 9;
     }
-    /*
-    timeSlot.each(function(){
-        var row = $(this);
-        var rowTime = $(this).attr("data-hour");
-        rowTime = parseInt(rowTime);
-        hour24 = parseInt(hour24);
-        //console.log(row);
-        console.log("rowTime: " + rowTime);
-        //console.log("hourOffset: " + hourOffset);
-        console.log("hour24: " + hour24);
-        if (rowTime < hour24) {
-            console.log("should be past");
-            // $hourRow.css('')
-            row.addClass("past");
-        }
-        else if (rowTime === hour24) {
-            console.log("should be present");
-            row.addClass("present");
-        }
-        else if (rowTime > hour24) {
-            console.log("should be future");
-            row.addClass("future");
-        } else {
-            console.log("for some reason, none of the condidtions were filled.");   
-        }
-    });
-    */
+    
     for(var i=0; i<timeSlot.length; i++) {
         var row = timeSlot[i];
         //console.log(row);
@@ -92,26 +66,7 @@ $(document).ready(function () {
             console.log("for some reason, none of the condidtions were filled.");   
         }
     };
-    /*
-    rowColor(timeSlot, hour)
-    // function to update row color
-    function rowColor(row, hour) {
-        //console.log(row);
-        //console.log(hour);
-        //console.log(hour24);
-        //console.log(rowTime);
-        if (hour < hour24) {
-            // $hourRow.css('')
-            row.css("background-color", "lightgrey")
-        }
-        else if (hour > hour24) {
-            row.css("background-color", "rgb(41, 92, 0)")
-        }
-        else {
-            row.css("background-color", "lightred")
-        }
-    };
-    */
+    
     // saves to local storage
     // onclick function to listen for user clicks on plan area
     $(document).on('click', 'i', function (event) {
